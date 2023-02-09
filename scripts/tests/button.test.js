a27995b607829c5557a1296ab8ab968095af5243
Kieran Collins <kiecol@protonmail.com>
2023-02-09 15:07:52 +0000

    Update forEach test with node fs module

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -4,13 +4,35 @@
 
 const buttonClick = require("/workspace/jest-test/scripts/tests/button");
 
-beforeEach( () => {
-    document.body.innerHTML = "<p id='par'></p>";
+// ----------------------------------------------------------------------------- Eaxample 1
+
+// beforeEach(() => {
+//     document.body.innerHTML = "<p id='par'></p>";
+// });
+
+// describe("DOM Tests", () => {
+//     test("expects p content to change", () => {
+//         buttonClick();
+//         expect(document.getElementById("par").innerHTML).toEqual("You clicked the button!");
+//     });
+// });
+
+// ----------------------------------------------------------------------------- Eaxample 2
+
+beforeEach(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM Tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You clicked the button!");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
